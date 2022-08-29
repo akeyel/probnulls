b8451a107615cea39c9502e05f3d6c1fd1184178
Keyel <akeyel@albany.edu>
2022-08-29 12:58:37 -0400

    Split out probnulls package from dfmip to stand on its own, and avoid the dependency issues in dfmip

---
+++ probnulls_builder.R
@@ -0,0 +1,8 @@
+## Code for maintaining the probnulls package
+# Create 2022-08-29
+# Author: Alexander C. Keyel alexander.keyel@health.ny.gov
+
+library(devtools)
+
+devtools::document() 
+devtools::build()
